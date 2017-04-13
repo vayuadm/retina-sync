d4888be93a85356b803e070d0ca1d48f31ca3f1b
ady <adymagal@gmail.com>
2017-04-13 14:42:22 +0300

    add test properties

--- src/test/java/adm/vayu/retina/sync/RetinaSyncTestCase.java
+++ src/test/java/adm/vayu/retina/sync/RetinaSyncTestCase.java
@@ -3,10 +3,12 @@ package adm.vayu.retina.sync;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("classpath:test.properties")
 public class RetinaSyncTestCase {
 }
